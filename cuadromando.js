38516a396365df63f69bc0ea7a4f6e6ed0271325
ElielMF <99572097+ElielMF@users.noreply.github.com>
2022-03-11 16:08:58 -0600

    Add files via upload

---
+++ cuadromando.js
@@ -0,0 +1,348 @@
+
+saldos=[["1410293002511","15/11/2012","11/11/2021","208.35","NAIRA BEATRIZ ALVARADO RAMIREZ","104.63","1616.8","1600","Falso","Verdadero","Falso"],
+["1410293002520","01/09/2008","01/05/2016","204.75","ARACELY NORMA HERNANDEZ RIVERA","0","1588","1600","Falso","Verdadero","Falso"],
+["1420290020409","20/06/2000","20/05/2020","150.15","AIDA CHAVEZ MONROY","4261.71","1151.2","1150","Verdadero","Verdadero","Falso"]]
+
+retiros=[["1420290020409","numero1","93002548","3885","15/03/2012"],
+["1420290020409","numero2","93002637","6996","20/12/2011"],
+["1410293002520","numero3","93002815","1900","21/12/2011"],["1420290020409","numero1","93002548","3885","15/03/2012"]]
+
+
+
+proyecion=[[ 0,850], [ 110,850], [ 111,850], [ 112,850], [ 113,850], [ 114,850], [ 115,850], [ 116,850], [ 117,850], [ 118,850], [ 119,850], [ 120,1000], [ 121,1000], [ 122,1000], [ 123,1000], [ 124,1000], [ 125,1000], [ 126,1000], [ 127,1000], [ 128,1000], [ 129,1000], [ 130,1000], [ 131,1000], [ 132,1000], [ 133,1000], [ 134,1000], [ 135,1000], [ 136,1000], [ 137,1000], [ 138,1000], [ 139,1000], [ 140,1000], [ 141,1150], [ 142,1150], [ 143,1150], [ 144,1150], [ 145,1150], [ 146,1150], [ 147,1150], [ 148,1150], [ 149,1150], [ 150,1150], [ 151,1150], [ 152,1150], [ 153,1150], [ 154,1150], [ 155,1150], [ 156,1150], [ 157,1150], [ 158,1150], [ 159,1150], [ 160,1150], [ 161,1300], [ 162,1300], [ 163,1300], [ 164,1300], [ 165,1300], [ 166,1300], [ 167,1300], [ 168,1300], [ 169,1300], [ 170,1300], [ 171,1300], [ 172,1300], [ 173,1300], [ 174,1300], [ 175,1300], [ 176,1300], [ 177,1300], [ 178,1300], [ 179,1300], [ 180,1300], [ 181,1450], [ 182,1450], [ 183,1450], [ 184,1450], [ 185,1450], [ 186,1450], [ 187,1450], [ 188,1450], [ 189,1450], [ 190,1450], [ 191,1450], [ 192,1450], [ 193,1450], [ 194,1450], [ 195,1450], [ 196,1450], [ 197,1450], [ 198,1450], [ 199,1450], [ 200,1450], [ 201,1600], [ 202,1600], [ 203,1600], [ 204,1600], [ 205,1600], [ 206,1600], [ 207,1600], [ 208,1600], [ 209,1600], [ 210,1600], [ 211,1600], [ 212,1600], [ 213,1600], [ 214,1600], [ 215,1600], [ 216,1600], [ 217,1600], [ 218,1600], [ 219,1600], [ 220,1600], [ 221,1750], [ 222,1750], [ 223,1750], [ 224,1750], [ 225,1750], [ 226,1750], [ 227,1750], [ 228,1750], [ 229,1750], [ 230,1750], [ 231,1750], [ 232,1750], [ 233,1750], [ 234,1750], [ 235,1750], [ 236,1750], [ 237,1750], [ 238,1750], [ 239,1750], [ 240,1750], [ 241,1900], [ 242,1900], [ 243,1900], [ 244,1900], [ 245,1900], [ 246,1900], [ 247,1900], [ 248,1900], [ 249,1900], [ 250,1900], [ 251,1900], [ 252,1900], [ 253,1900], [ 254,1900], [ 255,1900], [ 256,1900], [ 257,1900], [ 258,1900], [ 259,1900], [ 260,1900], [ 261,2050], [ 262,2050], [ 263,2050], [ 264,2050], [ 265,2050], [ 266,2050], [ 267,2050], [ 268,2050], [ 269,2050], [ 270,2050], [ 271,2050], [ 272,2050], [ 273,2050], [ 274,2050], [ 275,2050], [ 276,2050], [ 277,2050], [ 278,2050], [ 279,2050], [ 280,2050], [ 281,2200], [ 282,2200], [ 283,2200], [ 284,2200], [ 285,2200], [ 286,2200], [ 287,2200], [ 288,2200], [ 289,2200], [ 290,2200], [ 291,2200], [ 292,2200], [ 293,2200], [ 294,2200], [ 295,2200], [ 296,2200], [ 297,2200], [ 298,2200], [ 299,2200], [ 300,2200], [ 301,2350], [ 302,2350], [ 303,2350], [ 304,2350], [ 305,2350], [ 306,2350], [ 307,2350], [ 308,2350], [ 309,2350], [ 310,2350], [ 311,2350], [ 312,2350], [ 313,2350], [ 314,2350], [ 315,2350], [ 316,2350], [ 317,2350], [ 318,2350], [ 319,2350], [ 320,2350], [ 321,2500], [ 322,2500], [ 323,2500], [ 324,2500], [ 325,2500], [ 326,2500], [ 327,2500], [ 328,2500], [ 329,2500], [ 330,2500], [ 331,2500], [ 332,2500], [ 333,2500], [ 334,2500], [ 335,2500], [ 336,2500], [ 337,2500], [ 338,2500], [ 339,2500], [ 340,2500], [ 341,2650], [ 342,2650], [ 343,2650], [ 344,2650], [ 345,2650], [ 346,2650], [ 347,2650], [ 348,2650], [ 349,2650], [ 350,2650], [ 351,2650], [ 352,2650], [ 353,2650], [ 354,2650], [ 355,2650], [ 356,2650], [ 357,2650], [ 358,2650], [ 359,2650], [ 360,2650], [ 361,2800], [ 362,2800], [ 363,2800], [ 364,2800], [ 365,2800], [ 366,2800], [ 367,2800], [ 368,2800], [ 369,2800], [ 370,2800], [ 371,2800], [ 372,2800], [ 373,2800], [ 374,2800], [ 375,2800], [ 376,2800], [ 377,2800], [ 378,2800], [ 379,2800], [ 380,2800], [ 381,2950], [ 382,2950], [ 383,2950], [ 384,2950], [ 385,2950], [ 386,2950], [ 387,2950], [ 388,2950], [ 389,2950], [ 390,2950], [ 391,2950], [ 392,2950], [ 393,2950], [ 394,2950], [ 395,2950], [ 396,2950], [ 397,2950], [ 398,2950], [ 399,2950], [ 400,2950], [ 401,3100], [ 402,3100], [ 403,3100], [ 404,3100], [ 405,3100], [ 406,3100], [ 407,3100], [ 408,3100], [ 409,3100], [ 410,3100], [ 411,3100], [ 412,3100], [ 413,3100], [ 414,3100], [ 415,3100], [ 416,3100], [ 417,3100], [ 418,3100], [ 419,3100], [ 420,3100], [ 421,3250], [ 422,3250], [ 423,3250], [ 424,3250], [ 425,3250], [ 426,3250], [ 427,3250], [ 428,3250], [ 429,3250], [ 430,3250], [ 431,3250], [ 432,3250], [ 433,3250], [ 434,3250], [ 435,3250], [ 436,3250], [ 437,3250], [ 438,3250], [ 439,3250], [ 440,3250], [ 441,3400], [ 442,3400], [ 443,3400], [ 444,3400], [ 445,3400], [ 446,3400], [ 447,3400], [ 448,3400], [ 449,3400], [ 450,3400], [ 451,3400], [ 452,3400], [ 453,3400], [ 454,3400], [ 455,3400], [ 456,3400], [ 457,3400], [ 458,3400], [ 459,3400], [ 460,3400], [ 461,3550], [ 462,3550], [ 463,3550], [ 464,3550], [ 465,3550], [ 466,3550], [ 467,3550], [ 468,3550], [ 469,3550], [ 470,3550], [ 471,3550], [ 472,3550], [ 473,3550], [ 474,3550], [ 475,3550], [ 476,3550], [ 477,3550], [ 478,3550], [ 479,3550], [ 480,3550], [ 481,3700], [ 482,3700], [ 483,3700], [ 484,3700], [ 485,3700], [ 486,3700], [ 487,3700], [ 488,3700], [ 489,3700], [ 490,3700], [ 491,3700], [ 492,3700], [ 493,3700], [ 494,3700], [ 495,3700], [ 496,3700], [ 497,3700], [ 498,3700], [ 499,3700], [ 500,3700], [ 501,3850], [ 502,3850], [ 503,3850], [ 504,3850], [ 505,3850], [ 506,3850], [ 507,3850], [ 508,3850], [ 509,3850], [ 510,3850], [ 511,3850], [ 512,3850], [ 513,3850], [ 514,3850], [ 515,3850], [ 516,3850], [ 517,3850], [ 518,3850], [ 519,3850], [ 520,3850], [ 521,4000], [ 522,4000], [ 523,4000], [ 524,4000], [ 525,4000], [ 526,4000], [ 527,4000], [ 528,4000], [ 529,4000], [ 530,4000]]
+
+buscar= document.getElementById('Enviar_1');
+poliza= document.getElementById('poliza');
+
+vigencia= document.querySelector('.Vigencia');
+fefe= document.querySelector('.FEFE');
+desQuincenal= document.querySelector('.Desc_quinecenal');
+proyecionFinanciera= document.querySelector('.Proyecion_financiera');
+formula=document.querySelector('.formula ');
+saldo=document.querySelector('.saldo');
+nombre=document.querySelector('.nombre');
+
+caja_retiros=document.querySelector(".caja_retiros")
+caja_retiros_items=document.querySelector(".caja_retiros_items")
+
+
+busqueda_incremento=0
+
+buscar.addEventListener("click",()=>{
+	arrayretiros=[]
+	datosGenerales()
+
+	if (busqueda_incremento==0) {
+		ret()
+		caja_retiros_items.appendChild(fragmento)
+		retirosinf()
+		cajaInformacion()
+		busqueda_incremento=1
+	} else{
+		removerretiros()
+		ret()
+		caja_retiros_items.appendChild(fragmento)
+		retirosinf()
+		cajaInformacion()
+	}
+	
+})
+
+
+
+	
+
+datosGenerales=()=>{
+
+	for (elemento in saldos){
+		
+		
+
+		if (parseInt(saldos[elemento][0])==parseInt(poliza.value)) {
+
+			
+
+			FEMI=saldos[elemento][1]
+			FEFE=saldos[elemento][2]
+			D_QNAL=saldos[elemento][3]
+			NOMBRE=saldos[elemento][4]
+			SALDO_REAL_EN_POLIZA=saldos[elemento][5]
+			SALDO_A_1_AÑO=saldos[elemento][6]
+			
+
+			Verfiicacion_Saldo=saldos[elemento][7]
+			Verificacion_AÑO=saldos[elemento][8]
+			Verificacion_Mes=saldos[elemento][9]
+
+			saldo_formula=(((D_QNAL*24)/3)-50)
+
+				let saldo_proyeccion
+						
+						for (monto in proyecion){
+							if (Math.round(parseInt(D_QNAL))==proyecion[monto][0]) {
+								saldo_proyeccion=proyecion[monto][1]
+								break
+							}else{saldo_proyeccion="Utilizar Saldo calculado por formula"}
+						}
+
+
+
+			vigencia.innerHTML=FEMI;
+			fefe.innerHTML=FEFE;
+			desQuincenal.innerHTML=D_QNAL;
+			saldo.innerHTML=SALDO_REAL_EN_POLIZA
+			formula.innerHTML=saldo_formula
+			nombre.innerHTML=NOMBRE
+			nombre.style.color="white"
+			proyecionFinanciera.innerHTML=saldo_proyeccion
+
+
+			break
+		}
+		else{
+					FEMI="-----"
+					FEFE="----"
+					D_QNAL="----"
+					NOMBRE="Numero de poliza no econtado"
+					SALDO_REAL_EN_POLIZA="----"
+					SALDO_A_1_AÑO="----"
+					Verfiicacion_Saldo="----"
+					Verificacion_AÑO="----"
+					Verificacion_Mes="----"
+
+					vigencia.innerHTML=FEMI;
+					fefe.innerHTML=FEFE;
+					desQuincenal.innerHTML=D_QNAL;
+					saldo.innerHTML=SALDO_REAL_EN_POLIZA
+					nombre.innerHTML=NOMBRE
+
+					nombre.style.color="red"
+					
+			}
+	}	
+
+}
+
+fragmento=document.createDocumentFragment();
+
+let arrayretiros=[]
+
+ret=()=>{
+retiros_grupo=[]
+
+
+for (elemento in retiros){
+		
+		if (retiros[elemento][0]==poliza.value) {
+			arrayretiros.push(retiros[elemento][3]);
+			retiros_grupo.push(retiros[elemento]);			
+		}	
+	}
+
+		if (retiros_grupo.length>=1) {
+
+				for(element in retiros_grupo){
+					let monto_caja=document.createElement("DIV");
+					let monto_texto=document.createElement("P");
+					let fecha_caja=document.createElement("DIV");
+					let fecha_texto=document.createElement("P");
+
+
+					monto_texto.innerHTML=retiros_grupo[element][3];
+					fecha_texto.innerHTML=retiros_grupo[element][4];
+
+					monto_caja.appendChild(monto_texto);
+					fecha_caja.appendChild(fecha_texto);
+
+					fragmento.appendChild(monto_caja);
+					fragmento.appendChild(fecha_caja);
+
+					monto_caja.classList.add("item_retiro");
+					fecha_caja.classList.add("item_retiro")
+
+
+				}
+		}
+	
+}
+
+
+removerretiros=()=>{
+
+
+allretiros=document.querySelectorAll(".item_retiro");
+
+caja_retiros_items.remove()
+
+caja_retiros_items=document.createElement("DIV")
+caja_retiros_items.classList.add("caja_retiros_items")
+
+caja_retiros.appendChild(caja_retiros_items)
+
+}
+
+retirosinf=()=>{
+
+let sumretiros=0	
+	numretiros= (arrayretiros.length)
+
+	for (elemento in arrayretiros){
+		sumretiros += parseInt((arrayretiros[elemento]))
+		}	
+
+	sumret= document.getElementById('sumret');
+	numret=document.getElementById('numret');
+	sumretinf=document.querySelector('.sumretinf')
+
+	sumret.innerHTML=sumretiros;
+	numret.innerHTML=numretiros;
+	sumretinf.innerHTML=sumretiros
+
+
+}
+
+// CAJA INFROMACION
+
+cajaInformacion=()=>{
+let saldoformula= document.querySelector(".formula")
+let validacionSaldo
+let validacionSaldo_html= document.querySelector('.validacion_saldo')
+	
+// Validacion de saldo
+	if (parseInt(SALDO_REAL_EN_POLIZA)>(parseInt(saldoformula.innerHTML))){
+		validacionSaldo="Saldo suficiente para un año de dividendos"
+		validacionSaldo_html.style.background="rgb(19,92,157)"
+		}	else{
+					validacionSaldo="No cuenta con el saldo suficiente para un año de dividendos "
+					validacionSaldo_html.style.background="rgb(168,4,4)"
+				}
+validacionSaldo_html.innerHTML=validacionSaldo
+
+// Validacion de año
+{
+valAño=document.querySelector('.valAño')
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let fulldate=month+(year*12)+(day/30)
+
+let año=parseInt(FEMI.substring(6,10))
+let mes =parseInt(FEMI.substring(3,5))
+let dia = parseInt(FEMI.substring(0,2))
+let fullfecha=(mes)+(año*12)+(dia/30)
+
+let restfulls=(fulldate-fullfecha)/12
+
+	if (restfulls>=2) {
+		valAño.innerHTML="Periodo de espera ok";
+		valAño.style.background="rgb(19,92,157)"
+	}else{
+		valAño.innerHTML="No cumple lapso de tiempo necesario para cobrar dividendos"
+		valAño.style.background="rgb(168,4,4)"
+	}
+}
+{
+// Validacion  mes
+
+valMes=document.querySelector('.valMes')
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let fulldate=month+(year*12)+(day/30)
+
+let año=parseInt(FEFE.substring(6,10))
+let mes =parseInt(FEFE.substring(3,5))+2
+let dia = parseInt(FEFE.substring(0,2))
+let fullfecha=(mes)+(año*12)+(dia/30)
+
+restafulls=(fulldate-fullfecha)
+
+// Se esta considerando tres meses sin recibir añgun pago como atrasado
+
+
+	if (restafulls<=1) {
+		valMes.innerHTML="Se encuentra al corriente";
+		valMes.style.background="rgb(19,92,157)"
+	}else{
+		valMes.innerHTML="Esta atrasada con sus pagos"
+		valMes.style.background="rgb(168,4,4)"
+	}
+}
+
+
+
+{
+// Años transcurridos
+Transcurridos=document.querySelector('.Trancurridos')
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let fulldate=month+(year*12)+(day/30)
+
+let año=parseInt(FEMI.substring(6,10))
+let mes =parseInt(FEMI.substring(3,5))
+let dia = parseInt(FEMI.substring(0,2))
+let fullfecha=(mes)+(año*12)+((dia/30)+(.033))
+
+restafulls=(fulldate-fullfecha)/12
+
+	if (año<=2012){
+		Transcurridos.innerHTML=Math.floor(restafulls-.25)
+	}
+	else if (año==2013){
+		Transcurridos.innerHTML=Math.floor(restafulls-.5)
+	}
+	else{
+		Transcurridos.innerHTML=Math.floor(restafulls-1)
+	}
+}
+
+// Cobrados
+
+{
+let sumRet= document.getElementById('sumret')
+let saldoporaño= document.querySelector(".Proyecion_financiera")
+let Cobrados= document.querySelector(".Cobrados");
+
+Cobrados.innerHTML=parseInt(sumRet.innerHTML)/parseInt(saldoporaño.innerHTML)
+}
+
+// Podria Cobrar
+{
+let transcurridos= document.querySelector('.Trancurridos')
+let cobrados= document.querySelector(".Cobrados")
+let podriaCobrar= document.querySelector(".podriaCobrar");
+
+podriaCobrar.innerHTML=parseInt(transcurridos.innerHTML)-cobrados.innerHTML
+}
+
+{
+// Saldo por años
+
+let proyecion =document.querySelector('.proyeccion_')
+let formula =document.querySelector('.formula_')
+let transcurridos= document.querySelector('.Trancurridos')
+let saldoproyeccion= document.querySelector(".Proyecion_financiera")
+let saldoformula=document.querySelector(".formula")
+
+proyecion.innerHTML=parseInt(transcurridos.innerHTML)*(parseInt(saldoproyeccion.innerHTML))
+formula.innerHTML=parseInt(transcurridos.innerHTML)*(parseInt(saldoformula.innerHTML))
+}
+
+// Diferencia
+{
+	let proyecion =document.querySelector('.proyeccion_');
+	let formula =document.querySelector('.formula_');
+	let sumretinf=document.querySelector('.sumretinf');
+
+	let difproyecion=document.querySelector('.difproyec')
+	let difformula=document.querySelector('.difformula')
+
+	difproyecion.innerHTML=parseInt((proyecion.innerHTML))-parseInt((sumretinf.innerHTML))
+	difformula.innerHTML=parseInt((formula.innerHTML))-parseInt((sumretinf.innerHTML))
+}
+
+}
